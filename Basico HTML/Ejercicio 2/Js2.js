95c4626380ee4dcf3471bcfa0038b26fdd43e63b
Soy568 <155590557+Soy568@users.noreply.github.com>
2024-01-03 18:42:30 -0500

    Add files via upload

---
+++ Basico HTML/Ejercicio 2/Js2.js
@@ -0,0 +1,4 @@
+
+/* Concatenar dos Array A y B de la siguiente forma A0,B0,A1,B1,A2,B2.....
+    Los vectores tendran la misma longitud.
+*/
---
+++ Basico HTML/Js/array.js
@@ -0,0 +1,21 @@
+
+
+//Estructura de datos que permite almacenar un serie de Datos.
+
+var materias = ["Matematicas","Español","Ingles","Fisica","Arte"];
+
+console.log(materias);          // Muestra en consola los elementos del Arreglo.
+var nuevo = materias.length;    // Se crea una variable y se asigna el valor de la cantidad de elementos
+console.log(nuevo);             // Muestra en consola  el valor de nuevo.
+
+materias[3]="Calculo";          // Se muta el tercer 3 indice por el elemento Calculo.
+console.log(materias);          // Muestra en consola los elementos consus cambios actuales.
+
+
+var miListaDeCompras = [["Arroz",3],["Papa",5],["Azucar",6],["Tomate",2],["Pasta",1]];
+
+console.log(tamaño=miListaDeCompras.length);
+
+
+/*EJERCICIOS DE PRACTICA */
+/*Dado un vector de enteros, comprobar el mayor, el menor y la media de todos */
