4604a993ee10cb9210697719cfaae8c82afa68d2
Soy568 <155590557+Soy568@users.noreply.github.com>
2024-01-05 17:15:38 -0500

    Mejora de codigo

--- Basico HTML/Ejercicio 1/Js1.js
+++ Basico HTML/Ejercicio 1/Js1.js
@@ -1,41 +1,38 @@
+function Ejemplo(){
+    document.getElementById("text").value=" 5,69,83,123";    
+}
 
-function Ejemplo() {
-    document.getElementById("text").value="3,6,9";    //obtiene el elemento del ID (" ") y Mostrara el valor = " 3,6,9 "
-};
+function Reiniciar(){
+    location.reload();      //Funcion para reiniciar
+} 
 
-function Reiniciar() {
-    location.reload();              // Este metodo carga de nuevo la URL actual.
-};
-
-function Calcular() {
-    var Array = document.getElementById("text").value.split(/,/);           // El metodo split() divide el arreglo por ,
-
-    var mayor = -Infinity,menor=+Infinity,suma=0,media=0;           // La propiedad global Infinity es un valor numerico que representa el infinito.
-
-    // Calcular Suma
-    for(i=0; i<Array.length; i++){
-        suma = parseInt(Array[i])+suma;
+function Calcular(){
+    var array = document.getElementById("text").value.split(/,/);    
+    var mayor=-Infinity,menor=+Infinity,suma=0,media=0;
+    
+    //SUMA
+    for(i=0;i<array.length;i++){
+    suma=parseInt(array[i])+suma;
     }
-
-    // Calcular Media
-    media = suma/Array.length;
-
-    // Calcular Menor
-    for(i=0; i<Array.length; i++){
-        if(parseInt(Array[i]>menor))
-            menor = Array[i];
+    
+    //MEDIA
+    media=suma/array.length;
+    
+    //MENOR
+    for(i=0;i<array.length;i++){
+    if(parseInt(array[i])<menor) menor=array[i];
     }
-
-    // Calcular Mayor
-    for(i=0; i<Array.length; i++) {
-        if(parseInt(Array[i]>mayor))
-            mayor = Array[i];
+    
+    //MAYOR
+    for(i=0;i<array.length;i++){
+    if(parseInt(array[i])>mayor) mayor=array[i];
     }
-
-    // Calcular Resultado
-    document.getElementById("Resultado").innerHTML =        //Obtiene el elemento Id ("Resultado") y lo cambia por = " "
-    "La suma es : "+suma+"<br>"+
-    "La media es : "+media+"<br>"+
-    "El mayor es : "+mayor+"<br>"+
-    "El menor es : "+menor;
-};
+    
+    //RESULTADO
+    
+    document.getElementById("resultado").innerHTML="La Suma es : "+suma+"<br>"+
+                                                    "La Media es : "+media+"<br>"+
+                                                    "El Mayor es : "+mayor+"<br>"+
+                                                    "El Menor es : "+menor+"<br>"
+    
+}
