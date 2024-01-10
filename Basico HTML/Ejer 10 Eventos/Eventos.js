69e9770b93afb28e6ce8080abb624876c57f061f
Soy568 <155590557+Soy568@users.noreply.github.com>
2024-01-10 17:33:18 -0500

    mover mouse

--- Basico HTML/Ejer 10 Eventos/Eventos.js
+++ Basico HTML/Ejer 10 Eventos/Eventos.js
@@ -1,6 +1,21 @@
+/* Trabajando con dar clic */
+
 var Box = document.getElementById("Box");
 var Boton = document.getElementById("Boton");
 
 Boton.onclick = function() {
     Box.style.backgroundColor = "red";
 };
+
+
+/* Trabajando con mover mouse */
+var elemento = document.getElementById("miElemento");
+
+elemento.onmouseover = function(){
+    elemento.style.backgroundColor = "yellow";  //Paso el mouse
+};
+
+elemento.onmouseout = function() {
+    elemento.style.backgroundColor = "white";   //Retiro el mouse
+};
+
