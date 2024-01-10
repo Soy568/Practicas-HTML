ed39b1a62c33620eefea2886cdb44d340384b709
Soy568 <155590557+Soy568@users.noreply.github.com>
2024-01-10 08:40:39 -0500

    Trabajando con arreglos

---
+++ Basico HTML/Ejer 9 Estructura Datos/EstructuraDatos.js
@@ -0,0 +1,17 @@
+var colores = ["rojo","verde","azul","negro","blanco","gris"];
+console.log(colores);
+
+var peliculas2022=["El Gato con Botas","Sonic","Avatar 2","The Batman","Black Adam","Thor: Love and Thunder"];
+console.log(peliculas2022);
+
+
+var peliculas2023=["Barbie","Super Mario","Wonka","Aquaman y el Reino Perdido","The Nun 2"];    // Array inicial.
+console.log(peliculas2023);
+
+peliculas2023.push("Fast X","Blue Beetle","Elemental");                                         // Se agregan 3 peliculas al final del array.
+
+peliculas2022.push("Red","The house","Smile","Doctor Strange: Multiverso de la locura");
+peliculas2022.unshift("Pinocho","Black Panther","Minions");                                     // Se agregan 3 peliculas al inicio del array.
+
+
+/* ************************************************************************************************************ */
