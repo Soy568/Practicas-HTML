1d37f036d7df7f52762ef41a61a76c30b96f58ba
Soy568 <155590557+Soy568@users.noreply.github.com>
2024-01-12 16:08:47 -0500

    array

--- Basico HTML/Ejer 2 Concatenar Arrays/Js2.js
+++ Basico HTML/Ejer 2 Concatenar Arrays/Js2.js
@@ -2,3 +2,7 @@
 /* Concatenar dos Array A y B de la siguiente forma A0,B0,A1,B1,A2,B2.....
     Los vectores tendran la misma longitud.
 */
+
+function Ejemplo1() {
+    document.getElementById("Ejemplo1").value= " 15,598,3265,8 ";
+};
