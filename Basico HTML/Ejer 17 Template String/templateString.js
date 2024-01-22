1cd021b861f6965a293e617312e15a1c75e2c06d
Soy568 <155590557+Soy568@users.noreply.github.com>
2024-01-22 10:41:12 -0500

    template

---
+++ Basico HTML/Ejer 17 Template String/templateString.js
@@ -0,0 +1,16 @@
+
+var resultado
+var nombre ="Juana"
+var apellido="Manso"
+var edad=(28);
+
+resultado = `Pasado el tiempo, ${nombre} ${apellido} va a tener ${edad}`
+console.log(resultado)
+
+
+const nombres = "Carlos Castillo";
+const edades = 40;
+const pais = " Uruguay";
+
+console.log("La persona es " +nombres+ " tiene " +edades+ " y es de " +pais) ;      //Opcion 1
+console.log(`La persona es  ${nombres}  tiene  ${edades}  y es de ${pais}`);        //Opcion 2
