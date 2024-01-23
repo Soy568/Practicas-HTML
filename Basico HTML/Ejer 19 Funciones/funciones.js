59e963ac89fed4bd1f267f11a3f15700da2a98aa
Soy568 <155590557+Soy568@users.noreply.github.com>
2024-01-23 10:47:19 -0500

    funciones operaciones

--- Basico HTML/Ejer 19 Funciones/funciones.js
+++ Basico HTML/Ejer 19 Funciones/funciones.js
@@ -13,8 +13,6 @@ function SemaforoVerde(){
 };
 
 
-
-
 function Semaforo(color) {
     switch(color){
         case 'SemaforoRojo' :
@@ -29,4 +27,78 @@ function Semaforo(color) {
             console.log("Error Error");
     };
 };
-Semaforo("SemaforoRojo");
+Semaforo("SemaforoRojo");       //llamado a la funcion.
+
+
+
+/*Forma 1 */
+function saludo() {
+    console.log('Prueba de forma 1');
+};
+saludo();
+
+/*Forma 2 funcion flecha*/
+const saludo2 = () =>{
+    console.log('Prueba funcion flecha');
+};
+saludo2();
+
+const Prueba1 = (nombre) => {
+    console.log(`Prueba de nombre : ${nombre}`);
+};
+Prueba1('Pedro');
+Prueba1('Juan');
+Prueba1('Camilo');
+
+
+/* Calculadora */
+
+console.log('Operacion de suma y resta 1:');
+const operacion = (tipo,num1,num2)=> {
+    if(tipo === 'suma'){
+        console.log(num1+num2);
+    }else if (tipo === 'resta'){
+        console.log(num1-num2);
+    }
+};
+operacion('suma',10,30);
+operacion('resta',300,178);
+
+
+
+/* Return los resultados */
+
+console.log('Operacion de suma y resta 2:');
+const operacion1 = (tipo,num1,num2)=> {
+    let resultado;                  //variable 
+    if(tipo === 'suma'){
+        resultado = (num1+num2);        //asigna el resultado a la variable
+    }else if (tipo === 'resta'){
+        resultado = (num1-num2);        //asigna el resultado a la variable
+    }
+    return resultado;                   //retorna el resultado
+};
+const miresultado = operacion1('suma',40,50);   //asigno los argumentos a una variable
+console.log(miresultado);       //muestra el valor retornado por medio de la variable
+
+/* Return los resultados 2 */
+
+console.log('Operacion de suma y resta 3:');
+const operacion2 = (tipo,num1,num2)=> {
+    if(tipo === 'suma'){
+        return (num1+num2);        //asigna el resultado a la variable
+    }else if (tipo === 'resta'){
+        return (num1-num2);        //asigna el resultado a la variable
+    }                   
+};
+const resultado = operacion2('suma',320,560);   //asigno los argumentos a una variable
+console.log(resultado);       //muestra el valor retornado por medio de la variable
+
+
+/* Conocer el numero de letras de un nombre */
+
+let numeroLetras = (name) => {
+    let nume = name.length;
+    console.log(`${name} tiene ${nume} letras`);
+};
+numeroLetras('alejandro');
