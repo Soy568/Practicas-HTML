3d3d712cb6fd7149ed232c7cc3def2b6f44d101e
Soy568 <155590557+Soy568@users.noreply.github.com>
2024-01-29 07:34:51 -0500

    continue

--- Basico HTML/Ejer Break Continue/breakContinue.js
+++ Basico HTML/Ejer Break Continue/breakContinue.js
@@ -10,3 +10,16 @@ for(let i = 0; i < nombres.length; i++){
     console.log(nombres[i]);
 };
 
+/** Continue */
+/** La sentencia nos sirve para saltar a la siguiente iteracion.  */
+
+
+const invitados = ['Andres','Favio','Alba','Laura','Alberto','Maria','Anibal','Camila','Adriana','Braulio','Andrea','Arturo','Angel','Daniel','Miguel','Juan','Pedro'];
+console.log('Lista de personal con acceso: ');
+for(let i=0; i < invitados.length; i++){
+    if(invitados[i] === 'Daniel'){      // Si esta este valor
+        continue;                       // Salte ese valor y siga
+    };
+    console.log(invitados[i]);       
+};
+console.log(`Total de invitados ${invitados.length}`);
